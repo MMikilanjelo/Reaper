8bca145c664769d3dc7a5976e748585857e26dfa
qrie <qrie@ukr.net>
2023-10-02 10:00:07 +0300

    UI

--- World/Core.cs
+++ World/Core.cs
@@ -12,12 +12,6 @@ public partial class Core : Node2D
 	public override void _Ready()
 	{
 		
-		//worldGenerator.RunProceduralGeneration();
-		// tileMap.SetCell(0,new Vector2I(0,-10) ,0,new Vector2I(1 ,1 ),0);
-		// foreach(var direction in Directions.allDirectionsList)
-		// {
-		// 	tileMap.SetCell(0,new Vector2I(0,-10) + direction ,0,new Vector2I(3,3 ),0);
-		// }
 		 
 	}
 	
