64249acc06569aedb41b36fa8bf527748a7bcfbf
qrie <qrie@ukr.net>
2023-10-11 17:49:17 +0300

    dezine poison partickle

--- DotEffects/ToxicEffect/ToxicDotEffect.cs
+++ DotEffects/ToxicEffect/ToxicDotEffect.cs
@@ -9,9 +9,9 @@ namespace DotEffects
 	
 	public partial class ToxicDotEffect : BaseEffect 
 	{
-		PackedScene floatingTextScene;
 		[Export] TimerControllerComponent timer;
-		Label poisonCountersLabel;
+		[Export] PackedScene EffectPatrickle;
+		PackedScene floatingTextScene;
 		Timer total_effect_duration_timer;
 		Timer tick_effect_duration_timer;
 		
@@ -21,13 +21,13 @@ namespace DotEffects
 			effectStatsData = ResourceLoader.Load<EffectStats>("res://DotEffects/ToxicEffect/ToxicEfectData.tres");
 			total_count_of_effect = (int)(effectStatsData.EFFECT_TOTAL_DURATION/effectStatsData.EFFECT_TICK_DURATION);
 			floatingTextScene = ResourceLoader.Load("res://UI/FloatingText.tscn") as PackedScene;
-			poisonCountersLabel = GetNode<Label>("VBoxContainer/HBoxContainer/Label");
+			
 			total_effect_duration_timer= timer.CreateTimer(OneShoot: true);
 			tick_effect_duration_timer = timer.CreateTimer(OneShoot : true);
         }
         public override void ApplyEffect(StatusEfffectData _data)
         {
-			poisonCountersLabel.Text = total_count_of_effect.ToString();
+
 			total_effect_duration_timer.Start(effectStatsData.EFFECT_TOTAL_DURATION);
 			total_effect_duration_timer.Connect(Timer.SignalName.Timeout, Callable.From(()=> RemoveEffect(_data)));
 			tick_effect_duration_timer.Start(effectStatsData.EFFECT_TICK_DURATION);
@@ -38,18 +38,21 @@ namespace DotEffects
 			if(_data == null) {
 				return ;
 			}
+			
 			total_count_of_effect --;
 			_data?.healthComponent?.Damage(effectStatsData.EFFECT_TICK_DMG);
-			
-			//AddFloatingText(floatingTextScene , GlobalPosition , EFFECT_TICK_DMG.ToString());
-			poisonCountersLabel.Text = total_count_of_effect.ToString();
+			//AddFloatingText(floatingTextScene , GlobalPosition , effectStatsData.EFFECT_TICK_DMG.ToString());
+			AddPartcike(EffectPatrickle);
 			tick_effect_duration_timer.Start(effectStatsData.EFFECT_TICK_DURATION);
 			
 		}
 		public void RemoveEffect(StatusEfffectData _data)
 		{
+			
 			_data.healthComponent.Damage(effectStatsData.EFFECT_TICK_DMG);
-			//AddFloatingText(floatingTextScene , GlobalPosition , EFFECT_TICK_DMG.ToString());
+			
+			//AddFloatingText(floatingTextScene , GlobalPosition , effectStatsData.EFFECT_TICK_DMG.ToString());
+			AddPartcike(EffectPatrickle);
 			EmitSignal(SignalName.OnRemoveEfect);
 			QueueFree();
 		}
@@ -62,6 +65,13 @@ namespace DotEffects
 			floatText.Start(text);
 			
 		}
+		private void AddPartcike(PackedScene partickle)
+		{
+			GpuParticles2D 	patickles = partickle.Instantiate() as GpuParticles2D;
+			patickles.Position = Position;
+			AddChild(patickles);
+		}
+	
 
 
 
